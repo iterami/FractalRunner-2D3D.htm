77810af83f94203d0337a063f18710556127e662
Jan Horava <czechinthemail@gmail.com>
2015-08-15 14:40:41 -0700

    simplified contextAttributes usage

--- js/fractalrunner-2d3d.js
+++ js/fractalrunner-2d3d.js
@@ -356,12 +356,17 @@ function setmode(newmode, newgame){
 
             document.getElementById('page').innerHTML = '<canvas id=canvas></canvas><canvas id=buffer></canvas>';
 
-            buffer = document.getElementById('buffer').getContext('2d', {
+            var contextAttributes = {
               'alpha': false,
-            });
-            canvas = document.getElementById('canvas').getContext('2d', {
-              'alpha': false,
-            });
+            };
+            buffer = document.getElementById('buffer').getContext(
+              '2d',
+              contextAttributes
+            );
+            canvas = document.getElementById('canvas').getContext(
+              '2d',
+              contextAttributes
+            );
 
             resize();
         }
