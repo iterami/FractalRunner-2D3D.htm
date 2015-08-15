69c465cf3eef81d023259cf31f2f097e7f2413b3
Jan Horava <czechinthemail@gmail.com>
2015-08-15 14:50:09 -0700

    improved formatting

--- js/fractalrunner-2d3d.js
+++ js/fractalrunner-2d3d.js
@@ -354,7 +354,8 @@ function setmode(newmode, newgame){
         if(newgame){
             save();
 
-            document.getElementById('page').innerHTML = '<canvas id=canvas></canvas><canvas id=buffer></canvas>';
+            document.getElementById('page').innerHTML =
+              '<canvas id=canvas></canvas><canvas id=buffer></canvas>';
 
             var contextAttributes = {
               'alpha': false,
