3f36cd8f2f8fd7579896a5d01b657514e637307c
Jan Horava <czechinthemail@gmail.com>
2015-08-23 17:57:53 -0700

    moved disabled input css to css file

--- js/fractalrunner-2d3d.js
+++ js/fractalrunner-2d3d.js
@@ -387,7 +387,7 @@ function setmode(newmode, newgame){
 
     document.getElementById('page').innerHTML = '<div style=display:inline-block;text-align:left;vertical-align:top><div class=c><ul><li><a onclick="setmode(1, true)">Cling to the Ground</a><li><a onclick="setmode(2, true)">Walled Corridor</a></ul></div><hr><div class=c>Best: '
       + best
-      + '<br><a onclick=reset_best()>Reset Best</a></div></div><div style="border-left:8px solid #222;display:inline-block;text-align:left"><div class=c><input disabled style=border:0 value=ESC>Main Menu<br><input id=movement-keys maxlength=2 value='
+      + '<br><a onclick=reset_best()>Reset Best</a></div></div><div style="border-left:8px solid #222;display:inline-block;text-align:left"><div class=c><input disabled value=ESC>Main Menu<br><input id=movement-keys maxlength=2 value='
       + settings['movement-keys'] + '>Move ←→<br><input id=restart-key maxlength=1 value='
       + settings['restart-key'] + '>Restart</div><hr><div class=c><input id=audio-volume max=1 min=0 step=.01 type=range value='
       + settings['audio-volume'] + '>Audio<br><input id=ms-per-frame value='
