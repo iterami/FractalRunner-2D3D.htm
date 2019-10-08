9f7ebe795f2046d3b11f0652742dab43f46e1890
Jan Hořava <czechinthemail@gmail.com>
2019-10-07 18:42:51 -0700

    added repo_escape() for initial reset upon menu close

--- js/main.js
+++ js/main.js
@@ -205,6 +205,13 @@ function logic(){
     }
 }
 
+function repo_escape(){
+    if(split_state.length === 0
+      && !core_menu_open){
+        core_repo_reset();
+    }
+}
+
 function repo_init(){
     core_repo_init({
       'events': {
