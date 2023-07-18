71ae597111992293798f827bc89fc0a4786a9b46
Jan Hořava <czechinthemail@gmail.com>
2023-07-18 13:25:27 -0700

    combined js into repo.js

--- repo.js
+++ repo.js
@@ -1,5 +1,25 @@
 'use strict';
 
+function load_data(){
+    ground = canvas_properties['height-half'] * (1 - core_storage_data['level']);
+    player_position = 0;
+    score = 0;
+    split_state = [
+      false,
+      false,
+    ];
+    splits[0][2] = 25;
+    splits[1][2] = 25;
+    splits[2][2] = 25;
+    splits[3][2] = 25;
+
+    core_ui_update({
+      'ids': {
+        'score': score,
+      },
+    });
+}
+
 function repo_drawlogic(){
     canvas_setproperties({
       'properties': {
