65ec3eb4a6f02c1ef4a4d187c147f3d8e1b5848d
Jan Hořava <czechinthemail@gmail.com>
2025-05-03 01:21:28 -0700

    used mouse x position instead of movement for mobile control

--- repo.js
+++ repo.js
@@ -175,10 +175,11 @@ function repo_logic(){
     let move_right = false;
     if(core_mobile){
         if(core_mouse['down-0']){
-            if(core_mouse['movement-x'] > 0){
+            const x = core_mouse['x'] - canvas_properties['width-half'];
+            if(x > 0){
                 move_right = true;
 
-            }else if(core_mouse['movement-x'] < -0){
+            }else if(x < 0){
                 move_left = true;
             }
         }
